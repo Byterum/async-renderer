f65ac5934afad6e23b11f5de405003048befdd7b
Conlan Rios <conlan@gmail.com>
2019-12-19 16:59:56 -0800

    Initial hookup to blockchain for values

--- layered_static.js
+++ layered_static.js
@@ -1,6 +1,6 @@
 var Jimp = require('jimp');
 
-function render(layout, currentImage, layerIndex, callback) {
+function render(contract, layout, currentImage, layerIndex, callback) {
 	if (layerIndex >= layout.layers.length) {		
 		callback(currentImage)
 		return		
@@ -19,26 +19,36 @@ function render(layout, currentImage, layerIndex, callback) {
 
 	Jimp.read(layer.uri, (err, layerImage) => {
 		if (err) throw err;			
-			
-		if (currentImage !== null) {
-			var x = layer.x;
-			var y = layer.y;			
 
-			if (typeof x === "object") {
-				x = layer.x.value;
-			}
-	
-			if (typeof y === "object") {
-				y = layer.y.value;
-			}
-
-			currentImage.composite(layerImage, x, y);
-
-			render(layout, currentImage, layerIndex + 1, callback)
-		} else {
-			render(layout, layerImage, layerIndex + 1, callback)
-		}
+		OnImageRead(contract, currentImage, layout, layer, layerImage, layerIndex, callback);
 	})	
+}
+
+async function OnImageRead(contract, currentImage, layout, layer, layerImage, layerIndex, callback) {
+	if (currentImage !== null) {
+		var x = layer.x;
+		var y = layer.y;			
+
+		if (typeof x === "object") {
+			var controlToken = await contract.controlTokens(layer.x.token)
+			
+			x = parseInt(controlToken.currentValue.toString())
+			console.log(x)
+		}
+
+		if (typeof y === "object") {
+			var controlToken = await contract.controlTokens(layer.y.token)
+
+			y = parseInt(controlToken.currentValue.toString())
+			console.log(y)
+		}
+
+		currentImage.composite(layerImage, x, y);
+
+		render(contract, layout, currentImage, layerIndex + 1, callback)
+	} else {
+		render(contract, layout, layerImage, layerIndex + 1, callback)
+	}
 }
 
 exports.render = render
