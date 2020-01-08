e46a9edbac8728951843c8dcfccce184f1f035f3
Conlan Rios <conlan@gmail.com>
2020-01-07 19:01:37 -0800

    Add visible as a layer control

--- layered_static/v1.js
+++ layered_static/v1.js
@@ -4,7 +4,8 @@ const KEY_ROTATION = "rotation";
 const KEY_SCALE = "scale";
 const KEY_POSITION = "position";
 const KEY_X = "x";
-const KEY_Y = "y"
+const KEY_Y = "y";
+const KEY_VISIBLE = "visible";
 
 async function render(contract, layout, currentImage, layerIndex, callback) {
 	if (layerIndex >= layout.layers.length) {		
@@ -47,44 +48,53 @@ async function readIntProperty(contract, object, key, label) {
 
 async function OnImageRead(contract, currentImage, layout, layer, layerImage, layerIndex, callback) {
 	if (currentImage !== null) {
-		// scale the layer (optionally)
-		var x_offset = 0;
-		var y_offset = 0;
 
-		if (KEY_SCALE in layer) {
-			var scale_x = (await readIntProperty(contract, layer[KEY_SCALE], KEY_X, "Layer Scale X")) / 100;
-			var scale_y = (await readIntProperty(contract, layer[KEY_SCALE], KEY_Y, "Layer Scale Y")) / 100;
-		
-			// determine the new width
-			var newWidth = layerImage.bitmap.width * scale_x;
-			var newHeight = layerImage.bitmap.height * scale_y;
-			// determine the offset to maintain our position
-			x_offset = (layerImage.bitmap.width - newWidth) / 2;
-			y_offset = (layerImage.bitmap.height - newHeight) / 2;
-			// resize the image
-			layerImage.resize(newWidth, newHeight);
+		var isVisible = false;
+		// check if this layer has visbility controls
+		if (KEY_VISIBLE in layer) {
+			isVisible = (await readIntProperty(contract, layer, KEY_VISIBLE, "Layer Visible")) === 1;
 		}
 
-		// rotate the layer (optionally)
-		if (KEY_ROTATION in layer) {
-			var rotation = await readIntProperty(contract, layer, KEY_ROTATION, "Layer Rotation");
-			layerImage.rotate(rotation, false);
+		if (isVisible) {
+			// scale the layer (optionally)
+			var x_offset = 0;
+			var y_offset = 0;
+
+			if (KEY_SCALE in layer) {
+				var scale_x = (await readIntProperty(contract, layer[KEY_SCALE], KEY_X, "Layer Scale X")) / 100;
+				var scale_y = (await readIntProperty(contract, layer[KEY_SCALE], KEY_Y, "Layer Scale Y")) / 100;
+			
+				// determine the new width
+				var newWidth = layerImage.bitmap.width * scale_x;
+				var newHeight = layerImage.bitmap.height * scale_y;
+				// determine the offset to maintain our position
+				x_offset = (layerImage.bitmap.width - newWidth) / 2;
+				y_offset = (layerImage.bitmap.height - newHeight) / 2;
+				// resize the image
+				layerImage.resize(newWidth, newHeight);
+			}
+
+			// rotate the layer (optionally)
+			if (KEY_ROTATION in layer) {
+				var rotation = await readIntProperty(contract, layer, KEY_ROTATION, "Layer Rotation");
+				layerImage.rotate(rotation, false);
+			}
+
+			var x = 0;
+			var y = 0;
+
+			// position the layer (optionally)
+			if (KEY_POSITION in layer) {
+				x = await readIntProperty(contract, layer[KEY_POSITION], KEY_X, "Layer Position X");
+				y = await readIntProperty(contract, layer[KEY_POSITION], KEY_Y, "Layer Position Y");
+
+				x += x_offset;
+				y += y_offset;
+			}
+
+			// composite this layer onto the current image
+			currentImage.composite(layerImage, x, y);
 		}
-
-		var x = 0;
-		var y = 0;
-
-		// position the layer (optionally)
-		if (KEY_POSITION in layer) {
-			x = await readIntProperty(contract, layer[KEY_POSITION], KEY_X, "Layer Position X");
-			y = await readIntProperty(contract, layer[KEY_POSITION], KEY_Y, "Layer Position Y");
-
-			x += x_offset;
-			y += y_offset;
-		}
-
-		// composite this layer onto the current image
-		currentImage.composite(layerImage, x, y);
 
 		render(contract, layout, currentImage, layerIndex + 1, callback)
 	} else {
