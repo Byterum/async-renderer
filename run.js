7f1d3715d545714398fd356d637f75ec824ccda4
Conlan Rios <conlan@gmail.com>
2020-02-06 01:13:49 -0800

    Pull provider out of render.js

--- run.js
+++ run.js
@@ -1,4 +1,5 @@
 const renderer = require("./render.js")
+var ethers = require("ethers");
 
 // enforce that a file and token address was provided
 if (process.argv.length < 4) {
@@ -20,7 +21,14 @@ console.log("Using block = " + blockNum)
 async function main() {
 	var stampDebug = true;
 
-	var finalImageData = await renderer.process(tokenAddress, tokenId, blockNum, stampDebug);
+	// const provider = new ethers.providers.JsonRpcProvider('http://localhost:7545');
+	// const provider = new ethers.providers.JsonRpcProvider('https://goerli.infura.io/v3/687d696aa0a440ceadfb06bf931cfe06');
+	const provider = new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/687d696aa0a440ceadfb06bf931cfe06');
+
+	// const provider = new ethers.providers.JsonRpcProvider('https://www.ethercluster.com/goerli');
+	// const provider = new ethers.providers.InfuraProvider('goerli');
+
+	var finalImageData = await renderer.process(provider, tokenAddress, tokenId, blockNum, stampDebug);
 
 	// determine the render path
 	var path = "renders/token-" + tokenId + "_block-" + finalImageData.blockNum + ".png";
